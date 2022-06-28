4c11ccaf5cc7e9343bccba29f8f7f93ce5ace9ed
Konaistaken <107680957+Konaistaken@users.noreply.github.com>
2022-06-28 23:15:37 +0300

    Create hydra.lua

---
+++ hydra.lua
@@ -0,0 +1,51 @@
+local hydralauncher = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+local TextLabel_2 = Instance.new("TextLabel")
+local TextLabel_3 = Instance.new("TextLabel")
+
+hydralauncher.Name = "hydra launcher"
+hydralauncher.Parent = game.CoreGui
+
+Frame.Parent = hydralauncher
+Frame.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Frame.BorderColor3 = Color3.fromRGB(27, 42, 53)
+Frame.Position = UDim2.new(0.708530843, 0, 0.83853215, 0)
+Frame.Size = UDim2.new(0, 246, 0, 88)
+Frame.Style = Enum.FrameStyle.DropShadow
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Position = UDim2.new(0.151699901, 0, 0.068204917, 0)
+TextLabel.Size = UDim2.new(0, 155, 0, 26)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "hydra is fully injected"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+
+TextLabel_2.Parent = Frame
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.Position = UDim2.new(0.151699901, 0, 0.430157721, 0)
+TextLabel_2.Size = UDim2.new(0, 155, 0, 26)
+TextLabel_2.Font = Enum.Font.SourceSans
+TextLabel_2.Text = "Executor: Bubble Gum"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextScaled = true
+TextLabel_2.TextSize = 14.000
+TextLabel_2.TextWrapped = true
+
+TextLabel_3.Parent = Frame
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.BackgroundTransparency = 1.000
+TextLabel_3.Position = UDim2.new(0.00732794032, 0, 0.772656202, 0)
+TextLabel_3.Size = UDim2.new(0, 205, 0, 23)
+TextLabel_3.Font = Enum.Font.SourceSans
+TextLabel_3.Text = "Injector Made By Bubble Gum Softworks Team"
+TextLabel_3.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.TextScaled = true
+TextLabel_3.TextSize = 14.000
+TextLabel_3.TextWrapped = true
